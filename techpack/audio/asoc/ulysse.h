48d91915d50955dd0a6a1e68176bf02b5f776bec
R. Adi Permana <romadhonadipermana279f@gmail.com>
2020-08-26 02:34:47 +0700

    techpack: audio: Import Xiaomi changes for ulysse

---
+++ techpack/audio/asoc/ulysse.h
@@ -0,0 +1,7 @@
+#ifndef ULYSSE
+#define ULYSSE
+
+extern int msm_spk_ext_pa_ctrl(struct msm_asoc_mach_data *pdatadata, bool value);
+extern int msm_hs_ext_pa_ctrl(struct msm_asoc_mach_data *pdatadata, bool value);
+
+#endif
