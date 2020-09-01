dfda78822741bbf6aeffe2765495cc0b4f837ef0
R. Adi Permana <romadhonadipermana279f@gmail.com>
2020-09-02 04:02:28 +0700

    techpack: Import Xiaomi changes for ulysse

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -14966,6 +14966,13 @@ static const struct snd_kcontrol_new quin_mi2s_rx_switch_mixer_controls =
 	0, 1, 0, msm_routing_get_quin_mi2s_switch_mixer,
 	msm_routing_put_quin_mi2s_switch_mixer);
 
+#ifdef CONFIG_MACH_XIAOMI_ULYSSE
+static const struct snd_kcontrol_new quat_mi2s_fm_rx_switch_mixer_controls =
+		 SOC_SINGLE_EXT("Switch", SND_SOC_NOPM,
+		 0, 1, 0, msm_routing_get_quat_mi2s_switch_mixer,
+		 msm_routing_put_quat_mi2s_switch_mixer);
+#endif
+
 static const struct snd_kcontrol_new hfp_pri_aux_switch_mixer_controls =
 	SOC_SINGLE_EXT("Switch", SND_SOC_NOPM,
 	0, 1, 0, msm_routing_get_hfp_switch_mixer,
@@ -16813,6 +16820,10 @@ static const struct snd_soc_dapm_widget msm_qdsp6_widgets[] = {
 				&tert_mi2s_rx_switch_mixer_controls),
 	SND_SOC_DAPM_SWITCH("QUAT_MI2S_RX_DL_HL", SND_SOC_NOPM, 0, 0,
 				&quat_mi2s_rx_switch_mixer_controls),
+#ifdef CONFIG_MACH_XIAOMI_ULYSSE
+    SND_SOC_DAPM_SWITCH("QUAT_MI2S_FM_RX_DL_HL", SND_SOC_NOPM, 0, 0,
+                &quat_mi2s_fm_rx_switch_mixer_controls),
+#endif
 	SND_SOC_DAPM_SWITCH("QUIN_MI2S_RX_DL_HL", SND_SOC_NOPM, 0, 0,
 				&quin_mi2s_rx_switch_mixer_controls),
 	SND_SOC_DAPM_SWITCH("HFP_PRI_AUX_UL_HL", SND_SOC_NOPM, 0, 0,
@@ -19548,6 +19559,11 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"SEC_MI2S_RX", NULL, "SEC_MI2S_RX_DL_HL"},
 	{"TERT_MI2S_RX_DL_HL", "Switch", "TERT_MI2S_DL_HL"},
 	{"TERT_MI2S_RX", NULL, "TERT_MI2S_RX_DL_HL"},
+	
+#ifdef CONFIG_MACH_XIAOMI_ULYSSE
+    {"QUAT_MI2S_FM_RX_DL_HL", "Switch", "PRI_MI2S_DL_HL"},
+	{"QUAT_MI2S_RX", NULL, "QUAT_MI2S_FM_RX_DL_HL"},
+#endif
 
 	{"QUAT_MI2S_RX_DL_HL", "Switch", "QUAT_MI2S_DL_HL"},
 	{"QUAT_MI2S_RX", NULL, "QUAT_MI2S_RX_DL_HL"},
